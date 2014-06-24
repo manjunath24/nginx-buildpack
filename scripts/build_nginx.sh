38e5f4dee87eb6ffb606fcde75fbe559b65eb76f
manjunath24 <hugar.manjunathrt@gmail.com>
2014-06-24 12:57:54 +0530

    testing pagespeed

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -12,11 +12,13 @@
 NGINX_VERSION=1.5.7
 PCRE_VERSION=8.21
 HEADERS_MORE_VERSION=0.23
+NPS_VERSION=1.8.31.4
 
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
+pagespeed=https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
@@ -29,6 +31,9 @@ echo "Temp dir: $temp_dir"
 
 echo "Downloading $nginx_tarball_url"
 curl -L $nginx_tarball_url | tar xzv
+
+echo "Downloading PageSpeed"
+curl -L $pagespeed | tar xzv
 
 echo "Downloading $pcre_tarball_url"
 (cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
