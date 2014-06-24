eaebabe1815522a033bc33b9cb4eccc39b2a8c51
manjunath24 <hugar.manjunathrt@gmail.com>
2014-06-24 13:15:54 +0530

    configured pagespeed

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -19,6 +19,7 @@ nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://garr.dl.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
 headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
 pagespeed=https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.tar.gz
+pagespeed_psol=https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
@@ -35,6 +36,11 @@ curl -L $nginx_tarball_url | tar xzv
 echo "Downloading PageSpeed"
 curl -L $pagespeed | tar xzv
 
+cd ngx_pagespeed-release-${NPS_VERSION}-beta/
+
+echo "Downloading PageSpeed psol"
+curl -L $pagespeed_psol | tar xzv
+
 echo "Downloading $pcre_tarball_url"
 (cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
 
@@ -47,6 +53,8 @@ echo "Downloading $headers_more_nginx_module_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+        --add-module=/${temp_dir}/ngx_pagespeed-release-${NPS_VERSION}-beta
+    make
 	make install
 )
 
